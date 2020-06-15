5fd4a0fa06ad7edd9fdb7463ec123414fec1261c
Joshua Rose <Joshua.rose1@outlook.com>
2020-06-15 17:08:31 -0400

    add eslint 🚨

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
+        "import/extensions": [2, {
+            tsx: "never",
+            jsx: "never",
+            ts: "never",
+            js: "never",
+        }],
+        "max-len": [1, 200],
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            "vars": "all",
+            "args": "after-used",
+            "ignoreRestSiblings": false
+        }],
+        "react/no-array-index-key": 0,
+        "import/prefer-default-export": 1,
+        "react/prop-types": [1, {
+            ignore: [
+                'children',
+            ]
+        }]
+    }
+};
