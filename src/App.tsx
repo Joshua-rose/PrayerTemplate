e832685a375046eabd2fb3d13b02f9eaaaeae86f
Joshua Rose <Joshua.rose1@outlook.com>
2020-06-24 23:05:29 -0500

    Creating timer functions
    - advances to next section at end of timer but timer does not reset

--- src/App.tsx
+++ src/App.tsx
@@ -2,60 +2,92 @@
 /* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
 import Section from './components/section';
+import PrayerGuild, { plan } from './guides/prayerguide1';
 import logo from './logo.svg';
 import './App.css';
 
-function createInterval() {
-  return setInterval(() => {
-    // count down
-    // check if time past
+type sections = plan & {
+  isFocused?: boolean,
+  isComplete?: boolean,
+  isTimerRunning?: boolean,
 
-  }, 1000);
 }
 function ensureTwoDigit(val: number|string) {
   const valAsString = `${val}`;
   if (valAsString.length < 2) return `0${val}`;
   return valAsString;
 }
-const content = (
-  <div>
-    <p>Monocle ipsum dolor sit amet eclectic Baggu Singapore sleepy Scandinavian.Lovely bespoke business class Zürich Muji Tsutaya carefully curated international K - pop.Izakaya signature international alluring Sunspel boulevard Winkreative Beams lovely.Signature uniforms impeccable first - class hub, Lufthansa sophisticated.</p>
-
-    <p>Boulevard cosy Sunspel alluring Baggu Shinkansen.Carefully curated intricate sophisticated, Nordic Ettinger conversation joy hub Washlet punctual boutique.Boeing 787 Tsutaya Comme des Garçons Winkreative emerging impeccable Beams bulletin.Punctual smart bulletin, Ettinger global exclusive Fast Lane elegant handsome quality of life cosy first - class emerging airport Toto.</p>
-
-    <p>Airport lovely hub Beams extraordinary.Zürich Ginza Nordic iconic Scandinavian finest, bespoke exquisite Shinkansen Muji Helsinki Fast Lane.Conversation espresso smart Helsinki craftsmanship.Gaggenau signature charming Winkreative.</p>
-
-    <p>Liveable conversation concierge, destination first - class premium Fast Lane business class alluring Singapore Nordic.Comme des Garçons Gaggenau global tote bag, bureaux Swiss concierge.Airport Zürich elegant signature business class.Tote bag business class iconic Shinkansen Boeing 787 Winkreative, Muji Beams alluring boutique Toto.Charming Beams Washlet, Melbourne impeccable punctual Nordic artisanal iconic perfect Comme des Garçons Airbus A380 Fast Lane hand - crafted.Helsinki joy essential hand - crafted, hub soft power destination ANA uniforms artisanal vibrant impeccable business class Porter Ettinger.</p>
-  </div>
-);
 function App() {
   let interval: number;
-  const [time, setTime] = useState('00:00');
-  useEffect(() => {
+  const [time, setTime] = useState('');
+  const guideStarter: sections[] = [];
+  const [currentGuide, setCurrentGuide] = useState(guideStarter);
+  const [section, setSection] = useState(0);
+  const createInterval = (sectionTime: string) => {
     interval = setInterval(() => {
       setTime((t) => {
-        let [min, sec] = t.split(':').map((n) => parseInt(n, 10));
-        if (sec + 1 > 59) { min++; sec = 0; } else sec++;
-        return `${ensureTwoDigit(min)}:${ensureTwoDigit(sec)}`;
+        const tt = t !== '' ? t : sectionTime;
+        let [min, sec] = tt.split(':').map((n) => parseInt(n, 10));
+
+        if (--sec <= 0) {
+          if (min > 0) {
+            sec = 59; --min;
+          } else {
+            clearInterval(interval);
+            setSection((s) => {
+              const i = s + 1;
+              const guide = currentGuide;
+              guide[s].isFocused = false;
+              if (guide.length > i) {
+                guide[i].isFocused = true;
+              }
+              setCurrentGuide(guide);
+              return s;
+            });
+          }
+        }
+        return min >= 0 || sec >= 0 ? `${ensureTwoDigit(min)}:${ensureTwoDigit(sec)}` : '';
       });
-    }, 1000);
+    }, 100);
+  };
+  useEffect(() => {
+    const getContents = async () => {
+      const contents = await Promise.all(PrayerGuild.map((pg) => pg.content()));
+      const newPrayerGuild: sections[] = PrayerGuild.map((pg, i) => ({ ...pg, display: contents[i] }));
+      newPrayerGuild[section].isFocused = true;
+      setCurrentGuide(newPrayerGuild);
+    };
+    getContents();
     return () => {
       clearInterval(interval);
     };
   }, []);
+  useEffect(() => {
+    if (currentGuide.length > 0) {
+      clearInterval(interval);
+      const currentIndex = currentGuide.map((cg) => (cg.isFocused ? 'f' : '')).indexOf('f');
+      console.log(': -----------------------------------------------------------------------');
+      console.log('App -> currentGuide[currentIndex].time', currentGuide[currentIndex].time);
+      console.log(': -----------------------------------------------------------------------');
+      createInterval(currentGuide[currentIndex].time);
+    }
+  }, [currentGuide]);
 
   return (
     <div>
+      {currentGuide.map(({
+        title, display, time: length, isFocused, isComplete,
+      }) => (
+        <Section
+          title={title}
+          content={display}
+          isFocused={isFocused}
+          togglePlaying={() => {}}
+          proceedToNextSection={() => {}}
+          time={isFocused ? time : length}
+        />
 
-      <Section
-        title="Title"
-        content={content}
-        isFocused
-        isTimerRunning
-        togglePlaying={() => {}}
-        proceedToNextSection={() => {}}
-        time={time}
-      />
+      ))}
     </div>
   );
 }
